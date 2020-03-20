a0c7befa4f4632e09bd7586fac12cd02709e30cb
Shunsuke Okumura <shun.2fb.wb7@gmail.com>
2020-03-21 00:28:50 +0900

    Change style css -> scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ interface IUser {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'okazu-manager';
