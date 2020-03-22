f530016dce2e736ef24a0616c5fc8c3fe12d0e38
Shunsuke Okumura <shun.2fb.wb7@gmail.com>
2020-03-22 17:04:59 +0900

    Implement login and logout with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { TopPageComponent } from './top-page/top-page.component';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToTop = () => redirectLoggedInTo(['top']);
 
 const routes: Routes = [
   { path: '', component: TopPageComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectUnauthorizedToLogin} },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'top', component: TopPageComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectUnauthorizedToLogin} }
+  { path: 'login', component: LoginPageComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectLoggedInToTop} },
+  // { path: 'top', component: TopPageComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectUnauthorizedToLogin} }
 ];
 
 @NgModule({
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router
+    ) { }
 
   login() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(() => {
+      this.router.navigate(['/']);
+    });
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.afAuth.auth.signOut().then(() => {
+      this.router.navigate(['login']);
+    });
+  }
+
+  getUser(): Observable<firebase.User> {
+    return this.afAuth.user;
   }
 }
